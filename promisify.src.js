94379117e8f02be05f3af3604acc8024cc2d0423
Jesse Wertheim <jaawerth@users.noreply.github.com>
2016-06-20 09:08:49 -0400

    removed separate sub-functions for thisArg vs no thisArg.

--- promisify.src.js
+++ promisify.src.js
@@ -6,31 +6,14 @@ function create(Promise) {
   promisify.create = create; // So we can instantiate with compliant promises
 
   function promisify(nodebackFn, context) {
-    if (arguments.length >= 2) {
-      return boundPromisify(nodebackFn, context);
-    } else {
-      return unboundPromisify(nodebackFn);
-    }
-  }
-
-  function unboundPromisify(nodebackFn) {
+    const useContext = arguments.length >= 2;
     return function promisified(...args) {
-      return new Promise((resolve, reject) => {
-        args.push(function callback(err, res) {
-          if (err) reject(err); else resolve(res);
-        });
-        nodebackFn.apply(this, args);
-      });
-    };
-  }
-
-  function boundPromisify(nodebackFn, thisArg) {
-    return function promisfied(...args) {
+      const thisArg = useContext ? context : this;
       return new Promise((resolve ,reject) => {
         args.push(function callback(err, res) {
           if (err) reject(err); else resolve(res);
         });
-        nodebackFn.apply(thisArg, args);
+        Reflect.apply(nodebackFn, thisArg, args);
       });
     };
   }
